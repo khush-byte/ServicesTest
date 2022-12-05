70526f00c0d114f5e2481e03a4a31675ea94dcdf
khush-byte <khush4450@gmail.com>
2022-12-05 10:58:20 +0500

    notification lesson

--- app/src/main/java/com/example/servicestest/MainActivity.kt
+++ app/src/main/java/com/example/servicestest/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.servicestest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import com.example.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private var id = 0
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -15,7 +19,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.simpleService.setOnClickListener {
-            startService(MyService.newIntent(this))
+            startService(MyService.newIntent(this, 25))
         }
+
+        binding.foregroundService.setOnClickListener {
+            showNotification()
+        }
+    }
+
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Test title")
+            .setContentText("Test text")
+            .setSmallIcon(R.drawable.ic_baseline_beach_access)
+            .build()
+        notificationManager.notify(id++, notification)
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
     }
 }
--- app/src/main/java/com/example/servicestest/MyService.kt
+++ app/src/main/java/com/example/servicestest/MyService.kt
@@ -7,8 +7,7 @@ import android.os.IBinder
 import android.util.Log
 import kotlinx.coroutines.*
 
-class MyService: Service() {
-
+class MyService : Service() {
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     //Create service
@@ -20,14 +19,15 @@ class MyService: Service() {
     //Run services functions. This part use main flow
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         log("onStartCommand")
+        val start = intent?.getIntExtra(EXTRA_START, 0) ?: 0
         coroutineScope.launch {
-            for (i in 0 until 100){
+            for (i in start until start + 100) {
                 delay(1000)
                 log("Timer: $i")
             }
         }
-
-        return super.onStartCommand(intent, flags, startId)
+        //return super.onStartCommand(intent, flags, startId)
+        return START_STICKY
     }
 
     //Kill service
@@ -38,17 +38,20 @@ class MyService: Service() {
     }
 
     override fun onBind(intent: Intent?): IBinder? {
-            TODO("Not yet implemented")
+        TODO("Not yet implemented")
     }
 
-    private fun log(msg: String){
+    private fun log(msg: String) {
         Log.d("MyServices", "MyService: $msg")
     }
 
     //Fabric method
-    companion object{
-        fun newIntent(context: Context): Intent{
-            return Intent(context, MyService::class.java)
+    companion object {
+        private const val EXTRA_START = "start"
+        fun newIntent(context: Context, start: Int): Intent {
+            return Intent(context, MyService::class.java).apply {
+                putExtra(EXTRA_START, start)
+            }
         }
     }
 }
