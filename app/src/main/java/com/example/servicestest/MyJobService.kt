66e7abfa1afeb8fa26330e5f72d13edc70fae99a
khush-byte <khush4450@gmail.com>
2022-12-13 16:04:05 +0500

    job scheduler service - use enqueue

--- app/src/main/java/com/example/servicestest/MyJobService.kt
+++ app/src/main/java/com/example/servicestest/MyJobService.kt
@@ -2,6 +2,9 @@ package com.example.servicestest
 
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Intent
+import android.os.Build
+import android.os.PersistableBundle
 import android.util.Log
 import kotlinx.coroutines.*
 
@@ -15,13 +18,24 @@ class MyJobService : JobService() {
 
     override fun onStartJob(params: JobParameters?): Boolean {
         log("onStartCommand")
-        coroutineScope.launch {
-            for (i in 0 until 100) {
-                delay(1000)
-                log("Timer: $i")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = params?.dequeueWork()
+                while (workItem != null) {
+                    //return 0 if null
+                    //val page = params?.extras?.getInt(PAGE) ?: 0
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+                    for (i in 0 until 5) {
+                        delay(1000)
+                        log("Timer: $i $page")
+                    }
+                    params?.completeWork(workItem)
+                    workItem = params?.dequeueWork()
+                }
+                jobFinished(params, false)//finish service totally
             }
-            jobFinished(params, true)
         }
+
         return true
     }
 
@@ -44,5 +58,18 @@ class MyJobService : JobService() {
 
     companion object {
         const val JOB_ID = 111
+        private const val PAGE = "page"
+
+//        fun newBundle(page: Int): PersistableBundle{
+//            return PersistableBundle().apply {
+//                putInt(PAGE, page)
+//            }
+//        }
+
+        fun newIntent(page: Int): Intent{
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
     }
 }
